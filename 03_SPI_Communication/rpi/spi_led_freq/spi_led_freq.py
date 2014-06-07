692c62fa3c63343c2196a828ab4ba19d86842f8b
Cisco Zabala <x1sc0@acrobotic.com>
2014-06-07 16:00:46 +0000

    update

--- 03_SPI_Communication/rpi/spi_led_freq/spi_led_freq.py
+++ 03_SPI_Communication/rpi/spi_led_freq/spi_led_freq.py
@@ -1,11 +1,11 @@
 import spi
 
-status = spi.openSPI(speed=1000000)
+status = spi.openSPI(speed=500000)
 print "SPI configuration = ", status
 
 print "Reading from Arduinos:"
 
-dat = spi.transfer((1,3,5,6))
+dat = spi.transfer((1,3,5))
 for i in range(len(dat)):
     print dat[i]
 
